4ed121bc6037c1516835c82cf42a01fb693a189d
Marko Tiitto <marko.tiitto@hotmail.fi>
2023-01-16 19:36:52 +0200

    Updated version of Jobpost scraper
    Data is saved to a mongoDB database. Unnecessary code removed.

--- Duunitori_Scraper/V2_JobPost_Scraper_Duunitori.py
+++ Duunitori_Scraper/V2_JobPost_Scraper_Duunitori.py
@@ -3,7 +3,6 @@
 #creates or updates "democsv.csv" file on end  
 
 from datetime import datetime
-import re
 from bs4 import BeautifulSoup as bs
 import requests
 import functools as ft
@@ -11,6 +10,9 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import csv
+
+import json
+from pymongo import MongoClient
 
 #timer for run time
 start_time = time.time()
@@ -23,28 +25,25 @@ driver = webdriver.Firefox(executable_path=GeckoDriverManager().install()) """
 from webdriver_manager.chrome import ChromeDriverManager
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
+credPath = "./noExport/cred.json"
 
 def Main():
     #set how many pages are gone through | Each page has 20 jobs -> (1 + runCount)*20 = jobs scraped
-    runCount = 1
+    runCount = 9
     k = 0
-    totaltotalCount = 0
-    prevTotalCount = 0
-    mostKeywords = []
     pageCount = 0
 
     #KEYWORDS TO FIND
-    persistentSearchCount = {"python": 0, "javascript": 0, "c++": 0, "sql": 0, "flutter": 0}
+    persistentSearchCount = {"python": 0, "javascript": 0, "java": 0,
+                            "c++": 0, "sql": 0, "flutter": 0, "kotlin": 0,
+                            "php": 0, "c#": 0, "html": 0, "css": 0,
+                            "typescript": 0, "rust": 0, "swift": 0, "nosql": 0}
     keyString = persistentSearchCount.keys()
-    #Found numbers
-    salaries = []
 
     searchCount = {}
-    totalCount = 0
-    
 
     while k  <= runCount:
-            
+
         html_text = requests.get('https://duunitori.fi/tyopaikat?haku=Tieto-%20ja%20tietoliikennetekniikka%20(ala)&order_by=date_posted&sivu=' + str(k)).text
         soppa = bs(html_text, 'html.parser')
 
@@ -54,7 +53,7 @@ def Main():
             urlJob = link.get('href')
             print('https://duunitori.fi' + urlJob)
             driver.get('https://duunitori.fi' + urlJob)
-            driver.implicitly_wait(0.5)
+            driver.implicitly_wait(1)
             jobPost = driver.find_element(By.CLASS_NAME, 'description-box').text.lower()
 
             repls = ('\n', ''), ('<br>', '')
@@ -65,56 +64,35 @@ def Main():
                 searchCount[key] = jobPost.count(key)
                 persistentSearchCount[key] += searchCount[key]
             
-            #if all in searchCount empty skip
-            if any(searchCount.values()) :
-                #basic salary finder
-                if "palkka" in keyString:
-                    if(searchCount["palkka"] > 0):
-                        salaries = re.findall('[0-9]+', jobPost)
-                        #sort out least likely salary numbers
-                        salaries = list(filter(lambda a: 3 < len(a) < 6, salaries))
-
-                print("Possible salaries-> " + str(salaries))
-                print("KEYWORDS: ")
-                print(searchCount)
-
-                for count in searchCount.values():
-                    totalCount += count
-                print("TOTAL COUNT = " + str(totalCount))
-
-                totaltotalCount += totalCount
-                print("___________END____________")
-
-                if totalCount >= prevTotalCount:
-                    jobLink = 'https://duunitori.fi' + urlJob
-                    if totalCount == prevTotalCount:
-                        list(mostKeywords).append(jobLink)
-                    else:
-                        mostKeywords = jobLink
-                        prevTotalCount = totalCount
-
-            #empty for next job
-            totalCount = 0
-            salaries.clear()
             searchCount.clear()
             pageCount += 1
             
         k += 1
 
+    print(persistentSearchCount)
     driver.quit()
-    print("_____BEST job for you :)____ \n" + str(mostKeywords) + "\n________________________")
-    print("OVERALL COUNT = " + str(totaltotalCount))
     print("Process finished --- %s seconds ---" % (time.time() - start_time))
 
     AddToCSV(persistentSearchCount, keyString, pageCount)
+    ExportToMongo(persistentSearchCount)
 
 def AddToCSV(data: dict, keyString: dict, pages):
-    data.update({"Checked Pages": pages})
-    data.update({"Run Time": datetime.today().strftime("%d/%m/%Y|%H:%M:%S")})
+    data.update({"checkedPages": pages})
+    data.update({"runTime": datetime.today().strftime("%d/%m/%Y|%H:%M:%S")})
     with open("democsv.csv", "a") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames = keyString)
         writer.writeheader()
         writer.writerows([data]) 
     print(data)
+
+def ExportToMongo(data: dict):
+    file = open(credPath)
+    creds = json.load(file)
+
+    clusterConn = f"mongodb+srv://{creds['name']}:{creds['pass']}@scrapecluster.bvokbsk.mongodb.net/ScrapedData?retryWrites=true&w=majority"
+    client = MongoClient(clusterConn)
+    db = client.ScrapedData.Scrape1
+
+    db.insert_one(data)
 
 Main()
